03edeefa6c0749255bc98e772848dc3a488802c9
dialtamirano <143028005+dialtamirano@users.noreply.github.com>
2024-04-03 12:14:23 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,21 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+    //* to be implemented in part (a) */
+    int count = 0; 
+    for(int i = 0; i < myList.length; i++){
+      if(myList[i].size() == len){
+        count++;
+      }
   }
-
+    return count;
+  }
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
+    //* to be implemented in part (b) */
+  for(int j = 0; j < myList.length; j++){
+    if(myList[j].size() == len){
+      myList[j].remove();
   }
+}
 }
